35780395b1895501ff7ac4e8d7749f9ae670deb0
jiyu13 <jiyucoding@yahoo.com>
2023-09-12 20:59:02 -0400

    add image field

--- backend/myshop/models.py
+++ backend/myshop/models.py
@@ -16,7 +16,7 @@ class ProductImage(models.Model):
     """ A product has many images- ForeignKey """
     product = models.ForeignKey("Product", on_delete=models.CASCADE)
     image_name = models.CharField(max_length=120)
-    # image = models.ImageField(upload_to=)
+    product_image = models.ImageField(null=True, blank=True, upload_to='images/')
 
     def __str__(self):
         return self.image_name
