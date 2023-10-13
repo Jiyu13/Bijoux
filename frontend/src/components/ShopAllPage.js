694c8b4df589f01fd1d2466c4c54460d46a7f236
jiyu13 <jiyucoding@yahoo.com>
2023-10-12 22:45:49 -0400

    finish filter by materials

--- frontend/src/components/ShopAllPage.js
+++ frontend/src/components/ShopAllPage.js
@@ -18,36 +18,56 @@ export function ShopAllPage({products}) {
         const value = e.target.value ? e.target.value : e.currentTarget.value
 
         let filters = selectedFilters
-        if (e.target.checked) {
+        if (e.target.checked) {   // deal with checkbox
             filters = [...filters, value]
-        } else {
+        } else {                 // deal with selected filters showed below filterbox and unchecked checkbox
             filters = filters.filter(filter => filter !== value)
         }
 
-        // console.log(filters)
-
-        // ========================= filters excluding "sort" ================================================
-        let noSortFilters = filters.filter(f => !f.includes("Sort:"))
+        // ========================= filter by collections ================================================
         let filteredProducts = []
+        let sortFilters = filters.filter(f => f.includes("Sort:"))
+        let collectionFilters = filters.filter(f => f.includes("Collections:"))
+        let materialFilters = filters.filter(f => f.includes("Materials:"))
 
-        noSortFilters.forEach(f => {
+        collectionFilters.forEach(f => {
             const filterTerm = f.split(": ")[1]
-
-
             const results = [...products]?.filter(p => {
                 return p.collection.collection_name === filterTerm
             })
+
             filteredProducts = [...filteredProducts, ...results]
         })
 
-        // ========================= "sort" products ================================================
-        filteredProducts = noSortFilters.length > 0 ? filteredProducts : products
+        if (collectionFilters.length === 0) {
+            filteredProducts = products
+        }
 
-        if (filters.includes("Sort: Highest Price")) {
+        // =========================== filter by materials ======================================
+        let materialProducts = []
+        materialFilters.forEach(f => {
+            const filterTerm = f.split(": ")[1]
+            // filteredProducts is set to products if length===0
+            const results = [...filteredProducts]?.filter(p => {
+                return p.materials.some(material => material.material_name === filterTerm)
+            })
+
+            materialProducts = [...materialProducts, ...results]
+        })
+
+        if (materialFilters.length > 0)  {
+            // avoid adding duplicate products to the list when materialFilters is active
+            // if materialFilters is 0, materialProducts should be products
+            filteredProducts =  [...new Set(materialProducts)]
+        }
+
+        // ========================= "sort" products ================================================
+
+        if (sortFilters.includes("Sort: Highest Price")) {
             filteredProducts = filteredProducts.sort(function (a, b) {
                 return b.price - a.price
             })
-        } else if(filters.includes("Sort: Lowest Price")) {
+        } else if (sortFilters.includes("Sort: Lowest Price")) {
               filteredProducts = filteredProducts.sort(function (a, b) {
                 return a.price - b.price
             })
@@ -78,49 +98,3 @@ export function ShopAllPage({products}) {
 const ProductPageContainer = styled.div`
   margin: 1rem auto;
 `
-
-//
-// const getFilteredProducts = (f) =>{
-//     console.log(f)
-//     switch (f) {
-//         case "Sort: Highest Price":
-//             if (filteredProducts.length === 0) {
-//                 return [...products]?.sort(function (a, b) {
-//                     return b.price - a.price
-//                 })
-//             } else {
-//                 return [...filteredProducts]?.sort(function (a, b) {
-//                     return b.price - a.price
-//                 })
-//             }
-//         case "Sort: Lowest Price":
-//             if (filteredProducts.length === 0) {
-//                 return [...products]?.sort(function (a, b) { return a.price - b.price})
-//             } else {
-//                 return [...filteredProducts]?.sort(function (a, b) { return a.price - b.price})
-//             }
-//         case f.includes("Materials"):
-//             if (filteredProducts.length === 0 || filteredProducts.length === products.length) {
-//
-//                 const results = [...products]?.filter(p => {
-//                     console.log(p)
-//                     return p.materials.includes(filterTerm)
-//                 })
-//                 return results
-//             } else {
-//                 const results = [...products]?.filter(p => p.materials.includes(filterTerm))
-//                 return [...filteredProducts, ...results]
-//             }
-//         default:
-//
-//             if (filteredProducts.length === 0 || filteredProducts.length === products.length) {
-//                 const results = [...products]?.filter(p => p.collection.collection_name === filterTerm)
-//                 return results
-//             } else {
-//                 const results = [...products]?.filter(p => p.collection.collection_name === filterTerm)
-//                 return [...filteredProducts, ...results]
-//             }
-//     }
-// }
-// filteredProducts = getFilteredProducts(f)
-// console.log(filteredProducts)
