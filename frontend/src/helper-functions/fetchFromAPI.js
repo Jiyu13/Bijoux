ca927c351a65736b6595cdd81d29909171264a5c
jiyu13 <jiyucoding@yahoo.com>
2023-10-18 10:46:12 -0400

    add fn for posting

--- frontend/src/helper-functions/fetchFromAPI.js
+++ frontend/src/helper-functions/fetchFromAPI.js
@@ -1,40 +1,51 @@
 import axios from "axios";
 
 
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true
+
+export const client = axios.create({
+    baseURL: "http://127.0.0.1:8000/"
+})
+
+
+// const cors = require('cors');
+
 export const API_URL = "http://127.0.0.1:8000"
 export function fetchFromAPI(endpoint, setter) {
-    async function fetchData() {
-        // console.log(`${API_URL}${endpoint}`)
-        try {
-            const response = await axios.get(`${API_URL}${endpoint}`)
-            return response
-        } catch (error) {
+    // async function fetchData() {
+    //     // console.log(`${API_URL}${endpoint}`)
+    //     try {
+    //         const response = await axios.get(`${API_URL}${endpoint}`)
+    //         return response
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+    // fetchData().then(res => {
+    //     // console.log(res)
+    //     setter(res.data)
+    // })
+    client.get(`${endpoint}`, { withCredentials: true })
+        .then(res => {
+            // console.log(res)
+            setter(res.data)
+        })
+        .catch((error) => {
             console.log(error)
-        }
-    }
-    fetchData().then(res => {
-        // console.log(res)
-        setter(res.data)
-    })
+        })
 }
 
-// async function fetchData() {
-//     try {
-//         const response = await axios.get(`${API_URL}/categories/`)
-//         return response
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
-// fetchData().then(res => setCategories(res.data))
-
-
-// async function fetchData() {
-//     try {
-//         const response = await axios.get(`${API_URL}/carousels/`)
-//         return response
-//     } catch(error) {
-//         console.log(error)
-//     }
-// }
-// fetchData().then(res => setCarousels(res.data))
+export function postFromAPI(endpoint, data, setter, errorSetter) {
+    client.post(
+        `${endpoint}`,
+        data,
+        { withCredentials: true }
+    )
+    .then(res => {
+        // console.log(res.data)
+        setter(res.data)
+    })
+    .catch(error => errorSetter(error.response.data))
+}
