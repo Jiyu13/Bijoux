c1c342e4da9643bd092127d88dbb7280ee4a8f03
jiyu13 <jiyucoding@yahoo.com>
2023-10-19 09:19:31 -0400

    jsx change

--- frontend/src/account/AccountPage.js
+++ frontend/src/account/AccountPage.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import {useContext} from "react";
-import {UserContext} from "../../global/user-context/UserContext";
-import {client} from "../../helper-functions/fetchFromAPI";
+import {UserContext} from "../global/user-context/UserContext";
+import {client} from "../helper-functions/fetchFromAPI";
 import {useNavigate} from "react-router-dom";
 
 export function AccountPage() {
 
-    const { currentUser, setCurrentUser, setIsLogin} = useContext(UserContext)
+    const { currentUser, setCurrentUser, setIsLogin, isMobile, isTablet} = useContext(UserContext)
 
     // console.log(currentUser)
 
@@ -28,47 +28,77 @@ export function AccountPage() {
 
 
     return (
-        <AccountPageContainer>
+        <AccountPageContainer style={{margin: isMobile ? "0 16px" : ""}}>
             <div style={{
-                display: "flex",
-                justifyContent: "space-between",
+                // display: "flex",
+                // justifyContent: "space-between",
                 padding: "100px 0 35px",
-                borderBottom: "1px solid #dddddd"
+                // borderBottom: "1px solid #dddddd"
             }}>
                 <h1>Account</h1>
-                <LogoutText
-                    onClick={handleLogout}
-                >
+                <LogoutText onClick={handleLogout}>
                     Sign Out
                 </LogoutText>
             </div>
 
-            <UserInfo>
-                <div>
-                    <div>{userFullName}</div>
-                    <div>{userEmail}</div>
-                    <div>
-                        <a href="/account/addresses">
+            <UserInfo
+                style={{
+                    flexDirection: isMobile ? "column" : "row",
+
+                }}
+            >
+                <div style={{marginRight: "24px"}}>
+                    <h3>Account Details</h3>
+                    <InfoField>{userFullName}</InfoField>
+                    <InfoField>{userEmail}</InfoField>
+                    <InfoField style={{marginTop: "36px"}}>
+                        <ViewAddressLink href="/account/addresses">
                             View Addresses
-                        </a>
-                    </div>
+                        </ViewAddressLink>
+                    </InfoField>
                 </div>
-                <div></div>
+                <div>
+                    <h3>
+                        Orders
+                    </h3>
+                    <div>You haven't placed any orders yet.</div>
+                </div>
             </UserInfo>
         </AccountPageContainer>
     )
 }
 
-const AccountPageContainer = styled.div``
+const AccountPageContainer = styled.div`
+  //width: 85%;
+`
 const LogoutText = styled.div`
     margin: auto 0;
     cursor: pointer;
     color: rgb(82, 82, 82);
+    font-size: 0.875rem;
+    text-decoration-line: underline;
+    text-underline-offset: 0.3rem;
+  
     &:hover {
         text-decoration-line: underline;
         text-decoration-thickness: 2px;
     }
 `
 const UserInfo = styled.section`
-  
+  display: flex;
+  gap: 12px;
+`
+
+const InfoField = styled.div`
+  margin: 12px 0;
+`
+
+const ViewAddressLink = styled.a`
+  color: rgb(82, 82, 82);
+  font-size: 0.875rem;
+  text-underline-offset: 0.3rem;
+  &:hover {
+        text-decoration-line: underline;
+        text-decoration-thickness: 2px;
+  }
 `
