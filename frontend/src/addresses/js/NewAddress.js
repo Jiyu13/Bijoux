107397e34ee5347aee6be36e184a9a06f8e7deef
jiyu13 <jiyucoding@yahoo.com>
2023-10-23 15:45:27 -0400

    remove height css

--- frontend/src/addresses/js/NewAddress.js
+++ frontend/src/addresses/js/NewAddress.js
@@ -1,19 +1,70 @@
-import {Field, Input} from "../../account/CreateAccount";
+import {Field, Input, Label} from "../../account/CreateAccount";
 import {DarkButton, LightButton} from "../../components/buttons";
 import styled from "styled-components";
 
 import '../css/address.css'
 import {AddressLabel, FormHeader, NameFieldBox} from "../css/addressFormStyles";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import {UserContext} from "../../global/user-context/UserContext";
 
 export function NewAddress({setNewAddress}) {
 
     const {isMobile} = useContext(UserContext)
 
+    const initialValue = {
+        first_name: "",
+        last_name:  "",
+        address_line_1:  "",
+        address_line_2: "",
+        city: "",
+        country: "",
+        is_default: false,
+        phone: "",
+        state: "",
+        zip_code: ""
+    }
+
+    const [newFormData, setNewFormData] = useState(initialValue)
+
+    function handleOnchange(e) {
+        const name = e.target.name
+        let value
+        if (e.target.type === "checkbox") {
+            value = e.target.checked
+        } else {
+            value = e.target.value
+        }
+
+        setNewFormData({
+            ...newFormData,
+            [name]: value,
+        })
+    }
+
+    function handleNewAddressSubmit(e) {
+        e.preventDefault()
+
+        const formObject = {
+            first_name: newFormData.first_name,
+            last_name: newFormData.last_name,
+            address_line_1: newFormData.address_line_1,
+            address_line_2: newFormData.address_line_2,
+            city: newFormData.city,
+            country: newFormData.country,
+            is_default: newFormData.isDefault,
+            phone: newFormData.phone,
+            state: newFormData.state,
+            zip_code: newFormData.zip_code
+        }
+    }
+
     function handleCancel() {
         setNewAddress(false)
     }
+
+    const flexDirection = isMobile ? "column" : "row"
+    const gap = isMobile ? "0px" : "6px"
+    const width = isMobile ? "100%" : "50%"
 
     return (
     <NewFormPage>
@@ -24,56 +75,110 @@ export function NewAddress({setNewAddress}) {
 
                 <NameFieldBox
                     style={{
-                        flexDirection: isMobile ? "column" : "row",
-                        gap: isMobile ? "0px" : "6px"
+                        flexDirection: flexDirection,
+                        gap: gap
                     }}
                 >
-                    <Field style={{width: isMobile ? "100%" : "50%",}}>
+                    <Field style={{width: width}}>
                         <AddressLabel>First Name</AddressLabel>
-                        <Input type="text" value=""/>
+                        <Input
+                            type="text"
+                            name='first_name'
+                            value={newFormData.first_name}
+                            onChange={handleOnchange}
+                        />
 
                      </Field>
-                     <Field style={{width: isMobile ? "100%" : "50%",}}>
+                     <Field style={{width: width}}>
                         <AddressLabel>Last Name</AddressLabel>
-                        <Input type="text" value=""/>
+                        <Input
+                            type="text"
+                            name='last_name'
+                            value={newFormData.last_name}
+                            onChange={handleOnchange}
+                        />
 
                     </Field>
                 </NameFieldBox>
 
                 <Field>
                     <AddressLabel>Address line 1</AddressLabel>
-                    <Input />
-
+                    <Input
+                        type='text'
+                        name='address_line_1'
+                        value={newFormData.address_line_1}
+                        onChange={handleOnchange}
+                    />
                 </Field>
                 <Field>
                     <AddressLabel>Address line 2</AddressLabel>
-                    <Input/>
+                    <Input
+                        type="text"
+                        name='address_line_2'
+                        value={newFormData.address_line_2}
+                        onChange={handleOnchange}
+                    />
+                </Field>
+
+                    <Field>
+                        <AddressLabel>City</AddressLabel>
+                        <Input
+                            type="text"
+                            name="city"
+                            value={newFormData.city}
+                            onChange={handleOnchange}
+                        />
+                    </Field>
+
+                <Field>
+                    <AddressLabel>State / Province</AddressLabel>
+                    <Input
+                        type="text"
+                        name="state"
+                        value={newFormData.state}
+                        onChange={handleOnchange}
+                    />
                 </Field>
 
                 <Field>
-                    <AddressLabel>City</AddressLabel>
-                    <Input/>
-                </Field>
-                <Field>
                     <AddressLabel>Country</AddressLabel>
-                    <Input/>
+                    <Input
+                        type="text"
+                        name="country"
+                        value={newFormData.country}
+                        onChange={handleOnchange}
+                    />
                 </Field>
-                <Field>
-                    <AddressLabel>State / Province</AddressLabel>
-                    <Input/>
-                </Field>
+
+
                 <Field>
                     <AddressLabel>ZIP / Postal Code</AddressLabel>
-                    <Input/>
+                    <Input
+                        type="text"
+                        name="zip_code"
+                        value={newFormData.zip_code}
+                        onChange={handleOnchange}
+                    />
                 </Field>
+
                 <Field>
-                    <AddressLabel>Phone</AddressLabel>
-                    <Input/>
+                    <Label>Phone</Label>
+                    <Input
+                        type="text"
+                        name='phone'
+                        value={newFormData.phone}
+                        onChange={handleOnchange}
+                    />
                 </Field>
 
                 <Field style={{flexDirection: "row"}}>
 
-                    <Input type="checkbox" value="default address?"/>
+                    <Input
+                        type="checkbox"
+                        name='is_default'
+                        checked={newFormData.is_default}
+                        onChange={handleOnchange}
+                    />
                     <AddressLabel>Set as Default Address?</AddressLabel>
                 </Field>
                 <div style={{display: "flex", justifyContent: "center", gap: "2rem"}}>
