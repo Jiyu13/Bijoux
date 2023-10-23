e69095248fc97268885212b26ed1454af90a8bb7
jiyu13 <jiyucoding@yahoo.com>
2023-10-23 12:12:40 -0400

    css styles for address form

---
+++ frontend/src/addresses/css/addressFormStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import {Label} from "../../account/CreateAccount";
+
+
+
+export const AddressLabel = styled(Label)`
+  font-size: 0.8rem;
+  margin-bottom: 0.2rem
+`
+
+export const NameFieldBox = styled.div`
+    display: flex;
+`
+
+export const FormHeader = styled.h3`
+  margin: 12px 0 42px;
+`
