399bdfe6bb5dd5381e3564b1d44e4614a66c8ddd
jiyu13 <jiyucoding@yahoo.com>
2023-10-23 21:06:26 -0400

    style component

--- frontend/src/components/buttons.js
+++ frontend/src/components/buttons.js
@@ -18,12 +18,13 @@ export const LightButton = styled.button`
     background: none;
     color: rgb(82, 82, 82);
     padding: 12px 24px;
-    border: solid 1px #282c34;
+    border: solid 0.5px #282c34;
     letter-spacing: 0.1rem;
     transition: .3s ease;
     cursor: pointer;
     transition: .3s ease;
     margin: 2rem 0;
+    box-sizing: border-box;
     
     &:hover {
       box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 2px, rgb(51, 51, 51) 0px 0px 0px 2px;
