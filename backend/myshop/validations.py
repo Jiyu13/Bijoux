ef7b15fb59104a675084c4b210b78628659d2273
jiyu13 <jiyucoding@yahoo.com>
2023-10-28 19:38:17 -0400

    create errors variable to collect errors for each field; return errors if it's not empty

--- backend/myshop/validations.py
+++ backend/myshop/validations.py
@@ -4,22 +4,36 @@ UserModel = get_user_model()
 
 
 def custom_validation(data):
+
+    errors = {'errors': {}}
+
     email = data['email'].strip()
     first_name = data['first_name'].strip()
     last_name = data['last_name'].strip()
     password = data['password'].strip()
-    ##
-    if not email or UserModel.objects.filter(email=email).exists():
-        raise ValidationError('choose another email')
-    ##
-    if not password or len(password) < 8:
-        raise ValidationError('choose another password, min 8 characters')
-    ##
+
+    # ==================================================================
     if not first_name:
-        raise ValidationError('First name is required')
+        errors['errors']['first_name'] = 'First name is required'
     if not last_name:
-        raise ValidationError('Last name is required')
-    return data
+        errors['errors']['last_name'] = 'Last name is required'
+    # ==================================================================
+    if not email:
+        errors['errors']['email'] = 'Email is required.'
+    if UserModel.objects.filter(email=email).exists():
+        errors['errors']['email'] = 'Email is already taken.'
+    # ==================================================================
+    if not password or len(password) < 8:
+        errors['errors']['length'] = 'Password must be at least 8 characters long.'
+    if not any(char.isdigit() for char in password):
+        errors['errors']['numeric'] = 'Password must contain at least one numeric character.'
+    if not any(char.isalpha() for char in password):
+        errors['errors']['alphabetic'] = 'password must contain at least one alphabetic character.'
+
+    if errors['errors']:
+        return errors
+    else:
+        return data
 
 
 def validate_email(data):
@@ -40,4 +54,5 @@ def validate_password(data):
     password = data['password'].strip()
     if not password:
         raise ValidationError('a password is needed')
+    # if
     return True
