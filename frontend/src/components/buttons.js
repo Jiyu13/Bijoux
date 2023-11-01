d173ade517b2d8ad04d597501cd6084a00e4cbf6
jiyu13 <jiyucoding@yahoo.com>
2023-11-01 14:28:13 -0400

    move SubmitIputButton and CancelButton here

--- frontend/src/components/buttons.js
+++ frontend/src/components/buttons.js
@@ -1,5 +1,43 @@
 import styled from "styled-components";
 
+
+export const SubmitInputButton = styled.input`
+    background-color: rgba(40,44,52, 1);
+    color: whitesmoke;
+    padding: 12px 24px;
+    border: none;
+    letter-spacing: 0.1rem;
+    cursor: pointer;
+    transition: .3s ease;
+    margin: 2rem 0 2rem;
+    border-radius: 4px;
+    width: 100%;
+  
+    //border-radius: 4px;
+    //width: 100%;
+    &:hover {
+      box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 2px, rgb(51, 51, 51) 0px 0px 0px 2px;
+    }
+`
+
+export const CancelButton = styled.button`
+    background: none;
+    color: rgb(82, 82, 82);
+    padding: 12px 24px;
+    border: solid 0.5px #282c34;
+    letter-spacing: 0.1rem;
+    transition: .3s ease;
+    cursor: pointer;
+    transition: .3s ease;
+    margin: 2rem 0;
+    border-radius: 4px;
+    box-sizing: border-box;
+    
+    &:hover {
+      box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 2px, rgb(51, 51, 51) 0px 0px 0px 2px;
+    }
+`
+
 export const DarkButton = styled.button`
     background-color: rgba(40,44,52, 1);
     color: whitesmoke;
