fb6010770ed8b42c61a0c5cf4a648021a56254e5
jiyu13 <jiyucoding@yahoo.com>
2023-11-13 22:55:53 -0500

    show cartItemQuantity & update styled component css

--- frontend/src/cart/js/CartCountBubble.js
+++ frontend/src/cart/js/CartCountBubble.js
@@ -4,11 +4,11 @@ import styled from "styled-components";
 
 export function CartCountBubble() {
 
-    const {cartItems} = useContext(UserContext)
+    const {cartItemQuantity} = useContext(UserContext)
 
     return (
         <BubbleContainer>
-            <span>{cartItems?.length}</span>
+            <span>{cartItemQuantity}</span>
         </BubbleContainer>
     )
 }
@@ -17,13 +17,16 @@ const BubbleContainer = styled.div`
     position: absolute;
     background-color: rgba(40,44,52, 1);
     color: whitesmoke;
-    height: 1.2rem;
-    width: 1.2rem;
+    height: 1rem;
+    width: 1rem;
     border-radius: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
-  left: 2.2rem;
-  top: 0.5rem;
+    left: 2.3rem;
+    top: 0.8rem;
+    font-size: 0.6rem;
+    box-sizing: border-box;
+  font-weight: bold;
 `
 
