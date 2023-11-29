10a1ae014cec1cc9d9f3bf7864b43c6120c77221
jiyu13 <jiyucoding@yahoo.com>
2023-11-29 12:37:14 -0500

    move home component into a directory

--- frontend/src/pages/js/Home.js
+++ frontend/src/pages/js/Home.js
@@ -1,6 +1,6 @@
-import {Carousels} from "../carousels/js/Carousels";
-import {Collections2} from "../categories/js/Collections2";
-import {BestSellers} from "../best-sellers/js/BestSellers";
+import {Carousels} from "../../carousels/js/Carousels";
+import {Collections2} from "../../categories/js/Collections2";
+import {BestSellers} from "../../best-sellers/js/BestSellers";
 
 export function Home() {
 
