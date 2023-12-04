3891a3f4ddc50ff12ff7a721f73eb4e35a32064b
jiyu13 <jiyucoding@yahoo.com>
2023-12-04 15:44:20 -0500

    cart page body for anonymous users

--- frontend/src/cart/js/UserAnonymousCartItemsList.js
+++ frontend/src/cart/js/UserAnonymousCartItemsList.js
@@ -1,8 +1,17 @@
 import delete_icon from "../icons/delete.svg";
 import {CartItemQuantity} from "./CartItemQuantity";
-import styled from "styled-components";
 
-export function UserAnonymousCartItems({shoppingCartItems}) {
+import {
+    CartItemDetail,
+    CartItemImg,
+    CartItemsContainer,
+    CartItemWrapper,
+    DetailRow,
+    RowLeft,
+    RowRight
+} from "./UserCartItemsList";
+
+export function UserAnonymousCartItemsList({shoppingCartItems}) {
     return (
         <CartItemsContainer>
 
@@ -34,46 +43,42 @@ export function UserAnonymousCartItems({shoppingCartItems}) {
     )
 }
 
-const CartItemsContainer = styled.ul`
-  list-style: none;
-  padding-left: 0;
-  margin: 0px;
-  
-  box-sizing: border-box;
-  //height: calc(100% - 74px);
-  //overflow-y: scroll;
-`
-const CartItemWrapper = styled.li`
-  display: flex;
-  gap: 12px;
-  //margin: 12px 0;
-  padding: 16px 0px;
-  border-bottom: 1px solid rgb(234, 234, 234);
-  
-  &:last-child {
-    border-bottom: none;
-  }
-`
-const CartItemImg = styled.img`
-  width: 78px;
-  height: 78px;
-`
-
-const CartItemDetail = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  margin: auto 0;
-  gap: 12px;
-`
-const DetailRow = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const RowLeft = styled.div`
-  //left: 0;
-`
-const RowRight = styled.div`
-  //right: 0;
-`
+// const CartItemsContainer = styled.ul`
+//   list-style: none;
+//   padding-left: 0;
+//   margin: 0px;
+//
+//   box-sizing: border-box;
+//   //height: calc(100% - 74px);
+//   //overflow-y: scroll;
+// `
+// const CartItemWrapper = styled.li`
+//   display: flex;
+//   gap: 12px;
+//   //margin: 12px 0;
+//   padding: 16px 0px;
+//   border-bottom: 1px solid rgb(234, 234, 234);
+//
+//   &:last-child {
+//     border-bottom: none;
+//   }
+// `
+// const CartItemImg = styled.img`
+//   width: 78px;
+//   height: 78px;
+// `
+//
+// const CartItemDetail = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 100%;
+//   margin: auto 0;
+//   gap: 12px;
+// `
+// const DetailRow = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+// `
+//
+// const RowLeft = styled.div``
+// const RowRight = styled.div``
