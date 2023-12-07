5dd1af2c3b8d069ed46dc60cf6b5fa87a51aaee1
jiyu13 <jiyucoding@yahoo.com>
2023-12-06 21:39:01 -0500

    show loading_icon after clicking delete button; handle delete clicked item from localstorage shoppingCartItems

--- frontend/src/cart/js/UserAnonymousCartItemsList.js
+++ frontend/src/cart/js/UserAnonymousCartItemsList.js
@@ -1,30 +1,74 @@
 import delete_icon from "../icons/delete.svg";
 import {CartItemQuantity} from "./CartItemQuantity";
 
-import {
-    CartItemDetail,
-    CartItemImg,
-    CartItemsContainer,
-    CartItemWrapper,
-    DetailRow,
-    RowLeft,
-    RowRight
-} from "./UserCartItemsList";
+import { CartItemDetail, CartItemImg, CartItemsContainer, CartItemWrapper,
+    DeleteButton,  DetailRow, RowLeft, RowRight} from "./UserCartItemsList";
+import {useContext, useRef, useState} from "react";
+import {UserContext} from "../../global/user-context/UserContext";
+import loading_icon from "../icons/loading_icon.svg";
 
 export function UserAnonymousCartItemsList({shoppingCartItems}) {
+
+    const {setCartItemQuantity, setShoppingCartItems} = useContext(UserContext)
+
+    const [deletingItem, setDeletingItem] = useState(null)
+
+    function handleDeleteShoppingCartItem(e) {
+        const targetId =  parseInt(e.currentTarget.value)
+        setDeletingItem(targetId)
+
+        setTimeout(function() {
+            deleteFromCart(targetId)
+        }, 500)
+
+
+    }
+
+    function deleteFromCart(targetId) {
+        let deleteItemQuantity
+        try {
+            const updatedShoppingCartItems = shoppingCartItems.filter(item => {
+                if (item.product.product_id !== targetId) {
+                    return item
+                } else {
+                    deleteItemQuantity = item.quantity
+                }
+            })
+            setShoppingCartItems(updatedShoppingCartItems)
+            localStorage.setItem('shopping_cart_items', JSON.stringify(updatedShoppingCartItems))
+
+            setCartItemQuantity(prev => prev - deleteItemQuantity)
+
+        } catch(error) {
+            console.log("Product doesn't exist.")
+        }
+        finally {
+            setDeletingItem(null)
+        }
+    }
+
     return (
         <CartItemsContainer>
 
             {shoppingCartItems?.map((item, index) => {
+
                 return (
                     <CartItemWrapper key={index}>
                         <CartItemImg src={item.product.product_img} alt=""/>
                         <CartItemDetail>
                             <DetailRow >
                                 <RowLeft style={{fontSize: "1rem"}}>{item.product.product_title}</RowLeft>
-                                <RowRight style={{width: "20px"}}>
-                                    <img src={delete_icon} alt="delete icon" style={{width: "100%"}}/>
-                                </RowRight>
+
+                                {deletingItem === item.product.product_id ?
+                                    <RowRight style={{width: "20px", height: "20px"}}>
+                                        <img src={loading_icon} alt='loading icon' className='loading'/>
+                                    </RowRight>
+                                    :
+                                    <DeleteButton onClick={handleDeleteShoppingCartItem} value={item.product.product_id}>
+                                        <img src={delete_icon} alt="delete icon" style={{width: "100%"}}/>
+                                    </DeleteButton>
+                                }
+
                             </DetailRow>
 
                             <DetailRow>
