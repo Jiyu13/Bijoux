14b169602f769cfb414dfe4e879bbc9e33d14891
jiyu13 <jiyucoding@yahoo.com>
2023-12-06 21:08:27 -0500

    remove prop; pass in shoppingCartItems using useContext

--- frontend/src/cart/js/UserAnonymousCartBody.js
+++ frontend/src/cart/js/UserAnonymousCartBody.js
@@ -1,7 +1,12 @@
 import {EmptyCart} from "./EmptyCart";
 import {UserAnonymousCartItemsList} from "./UserAnonymousCartItemsList";
+import {useContext} from "react";
+import {UserContext} from "../../global/user-context/UserContext";
 
-export function UserAnonymousCartBody({shoppingCartItems}) {
+export function UserAnonymousCartBody() {
+
+    const {shoppingCartItems} = useContext(UserContext)
+
     return (
         <>
             {shoppingCartItems?.length === 0 ?
