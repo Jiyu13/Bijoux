6e04299cfb532db064e852ff22e593f69b2e0884
jiyu13 <jiyucoding@yahoo.com>
2023-12-09 16:46:45 -0500

    rename & add more links to the menu

--- frontend/src/global/header/HeaderMenuLinks.js
+++ frontend/src/global/header/HeaderMenuLinks.js
@@ -3,7 +3,7 @@ import "../navbar/css/navbar.css"
 
 import {NavLink} from "react-router-dom";
 
-export function HeaderCollectionMenuLinks() {
+export function HeaderMenuLinks() {
 
     const activeStyles = {
         textDecoration: "none",
@@ -42,6 +42,67 @@ export function HeaderCollectionMenuLinks() {
 
             <LinkItem>
                 <NavLink
+                    to="/earrings"
+                    style={ ({ isActive}) =>
+                        isActive ? activeStyles : normalStyles
+                    }
+                >
+                    Earrings
+                </NavLink>
+            </LinkItem>
+            <LinkItem>
+                <NavLink
+                    to="/rings"
+                    style={ ({ isActive}) =>
+                        isActive ? activeStyles : normalStyles
+                    }
+                >
+                    Rings
+                </NavLink>
+            </LinkItem>
+            <LinkItem>
+                <NavLink
+                    to="/necklaces"
+                    style={ ({ isActive}) =>
+                        isActive ? activeStyles : normalStyles
+                    }
+                >
+                    Necklaces
+                </NavLink>
+            </LinkItem>
+            <LinkItem>
+                <NavLink
+                    to="/bracelets"
+                    style={ ({ isActive}) =>
+                        isActive ? activeStyles : normalStyles
+                    }
+                >
+                    Bracelets
+                </NavLink>
+            </LinkItem>
+            <LinkItem>
+                <NavLink
+                    to="/new-arrivals"
+                    style={ ({ isActive}) =>
+                        isActive ? activeStyles : normalStyles
+                    }
+                >
+                    New Arrivals
+                </NavLink>
+            </LinkItem>
+            <LinkItem>
+                <NavLink
+                    to="/best-sellers"
+                    style={ ({ isActive}) =>
+                        isActive ? activeStyles : normalStyles
+                    }
+                >
+                    Best Sellers
+                </NavLink>
+            </LinkItem>
+
+            <LinkItem>
+                <NavLink
                     to="/contact"
                     style={ ({ isActive}) =>
                         isActive ? activeStyles : normalStyles
@@ -62,8 +123,19 @@ const MenuContainer = styled.ul`
   padding-left: 0;
   align-items: center;
   margin: 0;
+  height: 40px;
 `
 
 const LinkItem = styled.li`
-      margin: 0 10px;
+  margin: 0 10px;
+  white-space: nowrap;
+  
+  &:first-child {
+    margin-left: 0;
+  }
+  
+  &:hover {
+    text-decoration: underline;
+    text-underline-offset: 0.3rem;
+  }
 `
