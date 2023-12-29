f87c6a38dede5169631af5a3724eff78462ec0de
jiyu13 <jiyucoding@yahoo.com>
2023-12-29 11:13:19 -0500

    clean code

--- frontend/src/products/product-item-showcase/ProductItemShowcase.js
+++ frontend/src/products/product-item-showcase/ProductItemShowcase.js
@@ -2,21 +2,21 @@ import styled from "styled-components";
 import './product-item.css'
 
 
-import favorite_border_black_24dp from "./icons/favorite_border_black_24dp.svg"
-import favorite_white_24dp from "./icons/favorite_white_24dp.svg"
+// import favorite_border_black_24dp from "./icons/favorite_border_black_24dp.svg"
+// import favorite_white_24dp from "./icons/favorite_white_24dp.svg"
 
 import {useState} from "react";
-import {Navigate, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
 export function ProductItemShowcase( {product} ) {
-    const [isFav, setIsFav] = useState(false)
+    // const [isFav, setIsFav] = useState(false)
 
     // ======================= handle favourite icon ====================================
-    function handleAddFav() {
-        setIsFav((!isFav))
-    }
-    // #ffb142
-    const isFavColor = isFav ? "invert(86%) sepia(45%) saturate(3745%) hue-rotate(328deg) brightness(109%) contrast(102%)" : ""
+    // function handleAddFav() {
+    //     setIsFav((!isFav))
+    // }
+    // // #ffb142
+    // const isFavColor = isFav ? "invert(86%) sepia(45%) saturate(3745%) hue-rotate(328deg) brightness(109%) contrast(102%)" : ""
 
     // ======================= handle redirect to product detail page ====================================
     const navigate = useNavigate()
@@ -67,37 +67,36 @@ const Img = styled.img`
     width: 100%;
     //max-height: 250px;
 `
-const Circle = styled.div`
-  width: 40px;
-  height: 40px;
-  background-color: white;
-  position: absolute;
-  right: 0;
-  border-radius: 20px;
-  margin: 8px;
-  
-`
-const FavIcon = styled.img`
-  width: 24px;
-  cursor: pointer;
-  position: absolute;
-  right: 0;
-  margin: 8px;
-  padding: 8px;
-  border-radius: 20px;
-  &:active {
-    transform: translate3d(0, -2px, 0);
-  }
-`
+// const Circle = styled.div`
+//   width: 40px;
+//   height: 40px;
+//   background-color: white;
+//   position: absolute;
+//   right: 0;
+//   border-radius: 20px;
+//   margin: 8px;
+// `
+// const FavIcon = styled.img`
+//   width: 24px;
+//   cursor: pointer;
+//   position: absolute;
+//   right: 0;
+//   margin: 8px;
+//   padding: 8px;
+//   border-radius: 20px;
+//   &:active {
+//     transform: translate3d(0, -2px, 0);
+//   }
+// `
 
 const ProductContent = styled.a`
   cursor: pointer;
 `
 
-const ProductPromotion = styled.div`
-  font-size: 0.875rem;
-`
-const Promotion = styled.p``
+// const ProductPromotion = styled.div`
+//   font-size: 0.875rem;
+// `
+// const Promotion = styled.p``
 
 
 const ProductTitle = styled.h3`
@@ -107,16 +106,16 @@ const ProductTitle = styled.h3`
 const ProductPrice = styled.div`
   font-size: 16px;
 `
-const AddToCart = styled.div`
-    margin: 8px 0;
-`
-const AddToCartButton = styled.button`
-  height: 32px;
-  padding: 0 16px;
-  border: none;
-  transition: all .2s ease-in-out;
-  background-color: rgb(149, 175, 192, 0.5);
-    &:hover {
-        background-color: rgb(149, 175, 192);
-    }
-`
+// const AddToCart = styled.div`
+//     margin: 8px 0;
+// `
+// const AddToCartButton = styled.button`
+//   height: 32px;
+//   padding: 0 16px;
+//   border: none;
+//   transition: all .2s ease-in-out;
+//   background-color: rgb(149, 175, 192, 0.5);
+//     &:hover {
+//         background-color: rgb(149, 175, 192);
+//     }
+// `
